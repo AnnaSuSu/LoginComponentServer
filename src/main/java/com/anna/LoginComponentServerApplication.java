abcfc7af3fdfcb2f0322d3ecaa61845775589566
ansusu2001 <ansusu1025@gmail.com>
2022-05-25 15:05:39 +0800

    初始提交

---
+++ src/main/java/com/anna/LoginComponentServerApplication.java
@@ -0,0 +1,13 @@
+package com.anna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginComponentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginComponentServerApplication.class, args);
+    }
+
+}
